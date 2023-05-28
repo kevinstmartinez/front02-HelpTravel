59efd0723b8d3effb358e9b5369b6a1c56f7c019
Kevin Stiven Moreno Martinez <kmorenom1@ucentral.edu.co>
2023-05-27 22:17:29 -0500

    Adding more changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,7 @@ export default {
   ],
 
   server: {
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
   },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
